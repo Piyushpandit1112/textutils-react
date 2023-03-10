5b1ed801913cfebf2835b69f27a2d676789bcdfe
Piyush <piyushtiwary4141@gmail.com>
2023-03-10 11:05:10 +0530

    Updates

---
+++ precache-manifest.88eb964e746ba048b3c4d60b641fdbe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52b18f92660bc57529cd",
+    "url": "/textutils-react/static/css/main.5b99192c.chunk.css"
+  },
+  {
+    "revision": "52b18f92660bc57529cd",
+    "url": "/textutils-react/static/js/main.52b18f92.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils-react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "3e897f2210a78e65956d",
+    "url": "/textutils-react/static/js/runtime~main.3e897f22.js"
+  },
+  {
+    "revision": "886b1d83be9ac601ca3977433d3a876b",
+    "url": "/textutils-react/index.html"
+  }
+];
